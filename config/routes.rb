12f090f3ceb1c47227ed3eca35538908281548be
blueplanet <erguolinge@gmail.com>
2016-09-14 06:42:42 +0900

    r g scaffold enquets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :enquetes
   devise_for :users
+  root to: 'enquetes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
